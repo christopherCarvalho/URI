3215ba288536ec9751c47f76e3b3f8b1f7026d24
Christopher Cassius <christophercassius27@gmail.com>
2025-10-29 10:10:50 -0300

    URI 1004

---
+++ Ex1004.cpp
@@ -0,0 +1,16 @@
+//Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD.
+// A seguir mostre a variável PROD com mensagem correspondente. 
+#include <iostream>
+using namespace std;
+
+int main (){
+    int A,B,PROD;
+    
+    cin >> A >> B;
+
+    PROD = A * B;
+
+    cout << "PROD = " << PROD << endl;
+
+    return 0;
+}
